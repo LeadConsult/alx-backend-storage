c0aadfbfaa32405c45121290b1ab9115addb7b5c
LeadConsult <leadconsultnig@gmail.com>
2023-05-10 04:38:36 +0100

    0x01-NoSQL tasks

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+a Python function that changes all topics
+of a school document based on the name
+"""
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    name (string) will be the school name to update
+    topics (list of strings) will be the list of topics
+    approached in the school
+    """
+    return mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+a Python function that lists all documents in a collection
+"""
+import pymongo
+
+
+def list_all(mongo_collection):
+    """
+    return list in collections
+    """
+    if not mongo_collection:
+        return []
+    return list(mongo_collection.find())
